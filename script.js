1db7186d3be0c202477772f6386948440744aa5c
Ana <anamagpiee@gmail.com>
2022-10-27 23:52:20 +0200

    newport
    newport

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-let textoBoton = document.getElementById("btnCambiar").innerText;
-const clase = document.getElementById("galeria").innerText;
+$(window).scroll(function () {
+  parallax();
+});
 
+function parallax() {
+  var wscroll = $(window).scrollTop();
 
-function cambiarvista() {
-    if (galeria.className == "list") {
-        galeria.className = "grid";
-        textoBoton = document.getElementById("btnCambiar").innerText = "Modo lista";
-    } else {
-        galeria.className = "list";
-        textoBoton = document.getElementById("btnCambiar").innerText = "Modo cuadricula";
-    }
+  $(".parallax--bg").css(
+    "background-position",
+    "center " + wScroll * 0.75 + "px"
+  );
 }
 
+// Smooth Scrolling
+$(document).ready(function () {
+  var scrollLink = $(".scroll");
 
-/* añadir grid y list (png) al if y al else en js y al html (btn) para eliminar texto (innerText)
-innerHTML (reuerda meterlo antes en el HTML, a saber donde JA)*/
+  // Smooth scrolling
+  scrollLink.click(function (e) {
+    e.preventDefault();
+    $("body, html").animate(
+      {
+        scrollTop: $(this.hash).offset().top
+      },
+      1000
+    );
+  });
+
+  // Active link switching
+  $(window).scroll(function () {
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function () {
+      var sectionOffset = $(this.hash).offset().top;
+      if (sectionOffset <= scrollbarLocation) {
+        $(this).parent().addClass("active");
+        $(this).parent().siblings().removeClass("active");
+      }
+    });
+  });
+});
